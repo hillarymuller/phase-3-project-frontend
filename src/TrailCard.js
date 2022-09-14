4c1bec86d98eac7125e27640642d42a9a30bc949
Hillary Muller <hillaryaward@gmail.com>
2022-09-14 13:10:09 -0500

    favorite functionality

--- src/TrailCard.js
+++ src/TrailCard.js
@@ -1,5 +1,5 @@
 const TrailCard = ({ trail, setTrails }) => {
-    const { name, description, image, length, difficulty, id, park_id: parkId } = trail;
+    const { name, description, image, length, difficulty, id, park_id: parkId, favorite } = trail;
     function handleDelete(id) {
         fetch(`http://localhost:9292/parks/${parkId}/${id}`, {
             method: 'DELETE'
@@ -8,6 +8,21 @@ const TrailCard = ({ trail, setTrails }) => {
         .then((data) => setTrails(data))
         .catch(error => console.log(error))
     }
+    function handleFavorite(id) {
+        fetch(`http://localhost:9292/parks/${parkId}/${id}`, {
+            method: 'PATCH',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                favorite: !favorite,
+            }),
+        })
+        .then(r => r.json())
+        .then(data => setTrails(data))
+        .catch(error => console.log(error))
+    }
+
     return (
         <div className="card">
         <h3>{name}</h3>
@@ -17,9 +32,9 @@ const TrailCard = ({ trail, setTrails }) => {
         <span>{description}</span>
         <h4>length: {length}mi</h4>
         <h4>difficulty: {difficulty}</h4>
+        <button className="card-button" onClick={() => handleFavorite(id)}>{favorite ? "♥" : "♡"}</button>
         <button className="card-button" onClick={() => handleDelete(id)}>Delete Trail</button>
-        <br></br>
-
+        
     </div>
     )
 }
