a81479e799e6c35b69029dc88732c08ddb7e2c83
Hillary Muller <hillaryaward@gmail.com>
2022-09-21 13:46:41 -0500

    fix spacing on home page

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 const Home = () => {
 return (
-    <div>
+    <div className="card">
         <img src="https://images.unsplash.com/photo-1608671509237-ff7eb54fd1c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDN8fGRldmlscyUyMGxha2V8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60" alt="Devil's Lake State Park"></img>
     </div>
 )
